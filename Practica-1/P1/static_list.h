5de2532d095f2766fca4f14806206a6684be4510
alejandro.barrera <alejandro.barrera@udc.es>
2022-02-19 01:32:33 +0100

    modified:   dynamic_list.c 	modified:   dynamic_list.h 	modified:   main.c 	modified:   static_list.c 	modified:   static_list.h

--- Practica-1/P1/static_list.h
+++ Practica-1/P1/static_list.h
@@ -1,9 +1,9 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
+ * AUTHOR 1: Alejandro Barrera López LOGIN 1: alejandro.barrera
+ * AUTHOR 2: Álvaro Dolz del Castellar Castiñeira LOGIN 2: a.dolzdelcastellar1
+ * GROUP: 4.4
  * DATE: ** / ** / **
  */
 
