839c376414a0dad15cc5d8c7f46a22f56ae1a167
alejandro-barrera-lopez <alejandro.barrera@udc.es>
2022-03-28 09:18:30 +0200

    Enunciado Practica 2

---
+++ Practica-2/P2/product_list.h
@@ -0,0 +1,136 @@
+/*
+ * TITLE: PROGRAMMING II LABS
+ * SUBTITLE: Practical 2
+ * AUTHOR 1: ***************************** LOGIN 1: **********
+ * AUTHOR 2: ***************************** LOGIN 2: **********
+ * GROUP: *.*
+ * DATE: ** / ** / **
+ */
+
+#ifndef PRODUCT_LIST_H
+#define PRODUCT_LIST_H
+
+#include "types.h"
+#include <stdlib.h>		// Por malloc() y free()
+#include <stdbool.h>	// Para utilizar el tipo 'bool'
+#include <string.h>		// Por strcmp()
+
+#define LIST_SIZE_LIMIT 25
+#define LNULL NULL
+
+typedef struct tNode* tPosL;
+
+struct tNode {
+	tItemL dato;
+	tPosL siguiente;
+};
+
+typedef tPosL tList;
+
+/**
+ * Crea una lista vacia y la inicializa
+ * @return Una lista vacia
+ * @param lista Lista a comprobar
+ * @pos La salida sin datos
+ */
+void createEmptyList (tList*);
+
+/**
+ * Determina si la lista esta vacia
+ * @param lista Lista a comprobar
+ * @return True si la lista esta vacia, false en caso contrario
+ */
+bool isEmptyList (tList);
+
+/**
+ * Devuelve la posicion del primer elemento de la lista
+ * @param  lista que contiene el elemento
+ * @return Posicion del primer elemento
+ * @pre La lista no esta vacia
+ */
+tPosL first (tList);
+
+/**
+ * Devuelve la posicion del ultimo elemento de la lista
+ * @param lista Lista que contiene el elemento
+ * @return Posicion del primer elemento
+ * @pre La lista no esta vacia
+ */
+tPosL last (tList);
+
+/**
+ * Devuelve la posicion en la lista del elemento siguiente al actual
+ * @param posicion Posicion actual
+ * @param lista Lista que contiene el elemento
+ * @return Posicion del elemento siguiente, o nulo si es el ultimo
+ * @pre La lista no esta vacia
+ * @pre La posicion actual es una posicion valida de la lista
+ */
+tPosL next(tPosL, tList);
+
+/**
+ * Devuelve la posicion en la lista del elemento anterior al actual
+ * @param posicion Posicion actual
+ * @param lista Lista que contiene el elemento
+ * @return Posicion del elemento anterior, o nulo si es el primero
+ * @pre La lista no esta vacia
+ * @pre La posicion actual es una posicion valida de la lista
+ */
+tPosL previous(tPosL, tList);
+
+/**
+ * Añade un elemento a la lista en la posicion indicada, o al final si la posicion es nula
+ * @param elemento Elemento a añadir a la lista
+ * @param posicion Posicion en la que añadir el elemento a la lista
+ * @param lista Lista
+ * @return Lista con el elemento insertado
+ * @return True si se ha conseguido insertar, false en caso contrario
+ * @pre La posicion a introducir es valida o nula
+ * @pos Las posiciones anteriores a la del elemento insertado pueden haber variado
+ */
+bool insertItem (tItemL, tPosL, tList*);
+
+/**
+ * Elimina de la lista un elemento con cierta posicion
+ * @param posicion Posicion del elemento a eliminar
+ * @param lista Lista de la que eliminar el elemento
+ * @return Lista sin el elemento correspondiente
+ * @pre La posicion a introducir es valida
+ * @pos Las posiciones posteriores a la del elemento eliminado pueden haber variado
+ */
+void deleteAtPosition (tPosL, tList*);
+
+/**
+ * Devuelve el elemento en la posicion indicada de la lista
+ * @param posicion Posicion del elemento buscado
+ * @param lista Lista donde realizar la busqueda
+ * @return Elemento en la posicion indicada de la lista
+ * @pre La posicion a introducir es valida
+ */
+tItemL getItem (tPosL, tList);
+
+/**
+ * Modifica el contenido de un elemento de la lista
+ * @param elemento El nuevo elemento a modificar
+ * @param posicion La posicion del elemento modificar
+ * @param lista Lista en la que modificar el elemento
+ * @pre La posicion a introducir es valida
+ */
+void updateItem (tItemL, tPosL, tList*);
+
+/**
+ * Busca el primer elemento con cierto contenido en la lista
+ * @param producto Elemento a buscar
+ * @param lista Lista donde buscarlo
+ * @return Posicion donde se encuentra el elemento buscado, o nulo si no se encuentra
+ */
+tPosL findItem (tProductId, tList);
+
+/**
+ * Borra la lista, libera la memoria ocupada y restablece su tamaño y posicion a 0
+ * @param lista Lista a borrar
+ * @return La lista vacia
+ */
+void deleteList(tList* L);
+
+#endif
